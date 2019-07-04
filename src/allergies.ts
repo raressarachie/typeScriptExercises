0934405a2d90140391bf4843388d4fee17eea3e9
rares <raressarachie@gmail.com>
2019-07-04 17:52:50 +0300

    added comments

--- src/allergies.ts
+++ src/allergies.ts
@@ -1,8 +1,17 @@
+/*
+Allergies(score)
+{
+    allergiesMap - map for storing the know allergies key(score of the allergie), value(name of the allergie)
+    allergieList - the list for storing the person's allergies
+}
+*/ 
 class Allergies{
     
     public allergiesMap = new Map();
     public allergieList : string[] = [];
 
+    //constructor for the Allergies class 
+    //set the know allergies values and call computeList to populate allergieList
     constructor(public score: number = 0){
         this.allergiesMap.set(1, "eggs");
         this.allergiesMap.set(2, "peanuts");
@@ -15,11 +24,12 @@ class Allergies{
         this.computeList();
     }
 
+    //Function to find the highest power of 2 small than a give number
     public static highestPowerOfTwoSmallerThan(n: number){
         let res: number = 0; 
         for (let i=n; i>=1; i--) 
         { 
-            // If i is a power of 2 
+            // Checks i is a power of 2 
             if ((i & (i-1)) == 0) 
             { 
                 res = i; 
@@ -28,17 +38,21 @@ class Allergies{
         } 
         return res; 
         }
-
+    
+    //Function to compute the person's allergie list based on his score
     public computeList() : void{
 
         let tempScore : number = this.score;
         while(tempScore > 0)
-        {
+        {   
+            //compute the allergy with the highest score a person might have
             let alrg: number = Allergies.highestPowerOfTwoSmallerThan(tempScore);
+            
             if(alrg === 0)
                 break;
-
+            //check if it's a know allergy
             if(this.allergiesMap.get(alrg) !== undefined){
+                //put in the persons allergy list
                 this.allergieList.push(this.allergiesMap.get(alrg));
             }
 
@@ -46,10 +60,12 @@ class Allergies{
         }
     }
 
+    //function to list the persons allergies (they are stored in reverse because we start with the highest score)
     public list() :string[]{
         return this.allergieList.reverse();
     }
 
+    //function to check if a person is allergic to a given allergy
     public allergicTo(allergie : string) : boolean{
         return this.allergieList.includes(allergie);
     }
