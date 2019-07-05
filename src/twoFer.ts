5905e82d1778dc16f924ec1d6989beee7c25911c
rares <raressarachie@gmail.com>
2019-07-05 15:19:19 +0300

    solved one for you one for x

---
+++ src/twoFer.ts
@@ -0,0 +1,12 @@
+class TwoFer{
+    public static twoFer(name?: string) : string {
+        
+        if(name === undefined)
+        {
+            return `One for you, one for me.`;
+        }
+        else {return `One for ${name}, one for me.`;}
+    }
+}
+
+export default TwoFer
