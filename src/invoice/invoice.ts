6afdd84c9bb788b2165d20faa87c3c555df9c060
rares <raressarachie@gmail.com>
2019-07-08 18:07:15 +0300

    got started on the invoice

---
+++ src/invoice/invoice.ts
@@ -0,0 +1,22 @@
+class invoice{
+    constructor(private _total: number,private _client: string,private _date: Date){
+        this._total = _total;
+        this._client = _client;
+        this._date = _date;
+    }
+
+    get total(){
+        return this._total;
+    }
+
+    get client(){
+        return this._client;
+    }
+    
+    get date(){
+        return this._date;
+    }
+
+
+}
+export default invoice
---
+++ src/invoice/report.ts
@@ -0,0 +1,5 @@
+import invoice  from "./invoice";
+
+abstract class report{
+
+}
