914b5e376fc2c2a012fb06983d40692964126a35
rares <raressarachie@gmail.com>
2019-07-08 12:14:32 +0300

    added tests for the regex functions and completed the regex exercise succesfully

---
+++ src/regex.ts
@@ -0,0 +1,48 @@
+class regexValidators{
+    //function to validate is zip codes in ZIP and ZIP+4 format 11222 or 48034-1234
+    public static validateZipCodeUS(zip: string) :boolean{
+        let regexp: RegExp = /^\d{5}(?:[-\s]\d{4})?$/;
+        return regexp.test(zip);
+    }
+    //validate a phone number(romanian) ex +40 0773924110 10 digits must start with 0 followed by 7 
+    public static validatePhoneNumber(phoneNumber: string) :boolean{
+        let regexp: RegExp = /^\+40\s7\d{8}$/;
+        return regexp.test(phoneNumber);
+    }
+    private static formatMonth(month : number) : string{
+        month = month + 1;
+        if(month < 10)
+        {
+            return `0${month}`;
+        }
+        else return `${month}`;
+    }
+    private static formatDay(day : number) : string{
+        if(day < 10)
+        {
+            return `0${day}`;
+        }
+        else return `${day}`;
+    }
+
+    public static getToday() : string {
+        var today = new Date();
+        let date : string = today.getFullYear()+'-'+regexValidators.formatMonth(today.getMonth())+'-'+regexValidators.formatDay(today.getDate());//yyyy-mm-dd;  
+        return date;
+    }
+
+    //validate date of birth > 1900 <= today | dob entered as yyyy-mm-dd
+    public static validateDOB(dob: string) :boolean{
+        var today = new Date();
+        let year : string = String(today.getFullYear());
+        let month : string = regexValidators.formatMonth(today.getMonth());
+        let day : string = regexValidators.formatDay(today.getDate());
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();//yyyy-mm-dd;
+        let hardcoded = new RegExp('^(?:2019-07-0[1-8]|2019-(?:02-(?:[12][0-9]|0[1-9])|0[46]-(?:30|[12][0-9]|0[1-9])|0[135]-(?:3[01]|[12][0-9]|0[1-9]))|(?:201[0-8]|200[0-9]|19[0-9]{2})-(?:02-(?:[12][0-9]|0[1-9])|(?:0[469]|11)-(?:30|[12][0-9]|0[1-9])|(?:0[13578]|1[02])-(?:3[01]|[12][0-9]|0[1-9])))$');
+        let regDate = new RegExp(`^(?:${year}-${month}-${day.charAt(0)}[1-${day.charAt(1)}]|${year}-(?:02-(?:[12][0-9]|0[1-9])|0[46]-(?:30|[12][0-9]|0[1-9])|0[135]-(?:3[01]|[12][0-9]|0[1-9]))|(?:20${year.charAt(2)}[0-${year.charAt(3)}]|200[0-9]|19[0-9]{2})-(?:02-(?:[12][0-9]|0[1-9])|(?:0[469]|11)-(?:30|[12][0-9]|0[1-9])|(?:0[13578]|1[02])-(?:3[01]|[12][0-9]|0[1-9])))$`);
+        return regDate.test(dob);
+    }
+
+}
+
+export default regexValidators;
---
+++ tests/regextTest.ts
@@ -0,0 +1,91 @@
+import regexValidators from '../src/regex'
+
+describe('regexValidators', () => {
+    
+    describe('validate zip codes USA', () => {
+        it('valid ZIP format', () => {
+          expect(regexValidators.validateZipCodeUS('12345')).toBeTruthy();
+        });
+
+        it('valid ZIP+4 format', () => {
+            expect(regexValidators.validateZipCodeUS('12345-1234')).toBeTruthy();
+        });
+
+        it('invalid ZIP format', () => {
+            expect(regexValidators.validateZipCodeUS('123456')).toBeFalsy();
+        });
+
+        it('invalid ZIP+4 format', () => {
+            expect(regexValidators.validateZipCodeUS('12345-12345')).toBeFalsy();
+        });
+
+        it('no hyphen in ZIP+4', () => {
+            expect(regexValidators.validateZipCodeUS('12345+1234')).toBeFalsy();
+        });
+
+        it('empty ZIP', () => {
+            expect(regexValidators.validateZipCodeUS('')).toBeFalsy();
+        });
+        
+    });
+
+    describe('validate romanian phone number', () => {
+        it('valid phone number', () => {
+            expect(regexValidators.validatePhoneNumber('+40 744549812')).toBeTruthy();
+        });
+        it('invalid prefix', () => {
+            expect(regexValidators.validatePhoneNumber('+41 744549812')).toBeFalsy();
+        });
+        it('phone number to long', () => {
+            expect(regexValidators.validatePhoneNumber('+40 7441549812')).toBeFalsy();
+        });
+
+        it('no prefix', () => {
+            expect(regexValidators.validatePhoneNumber('07441549812')).toBeFalsy();
+        });
+        it('empty phone number', () => {
+            expect(regexValidators.validatePhoneNumber('')).toBeFalsy();
+        });
+    });
+
+    describe('validate date of birth must be between 1900-today, in the YYYY-MM-DD format', () => {
+        it('valid date of birth', () => {
+            expect(regexValidators.validateDOB('1998-11-02')).toBeTruthy();
+        });
+        it('dob < 1900', () => {
+            expect(regexValidators.validateDOB('1899-11-02')).toBeFalsy();
+        });
+        it('dob today', () => {
+            let today : string = regexValidators.getToday(); 
+            expect(regexValidators.validateDOB(today)).toBeTruthy();
+        });
+        it('dob one year after today', () => {
+            let today : string[] = regexValidators.getToday().split('-');
+            
+            let afterToday : string = String(Number(today[0])+1) + today[1] + today[2]; 
+            expect(regexValidators.validateDOB(afterToday)).toBeFalsy();
+        });
+        it('dob one month after today(if todays month not december)', () => {
+            let today : string[] = regexValidators.getToday().split('-');
+            
+            if(Number(today[1]) < 12){
+                let afterToday : string = today[0] + String(Number(today[1])+1) + today[2]; 
+                expect(regexValidators.validateDOB(afterToday)).toBeFalsy();}
+        });
+        it('dob invalid day', () => {
+            expect(regexValidators.validateDOB('2000-07-33')).toBeFalsy();
+        });
+
+        it('dob invalid month', () => {
+            expect(regexValidators.validateDOB('2000-13-33')).toBeFalsy();
+        });
+
+        it('dob invalid day in february(30)', () => {
+            expect(regexValidators.validateDOB('2000-02-30')).toBeFalsy();
+        });
+        it('dob invalid day in november(31)', () => {
+            expect(regexValidators.validateDOB('2000-11-31')).toBeFalsy();
+        });
+
+    });
+})
